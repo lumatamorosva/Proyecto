d27b62e7860930183c49dc5dbd02ad72d4f7e859
lumatamorosva <lumatamorosva@est.utn.ac.cr>
2025-03-29 10:45:21 -0600

    PagUbicacionConMapa

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-document.getElementById("test1").innerText("23");
+/*Geolocation API*/
+function initMap() {
+    // Verifica si el navegador soporta la geolocalización
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const lat = 9.994909;
+            const lon = -84.230364;
+            const shopLocation = { lat: lat, lng: lon };
+
+            // Crear el mapa centrado
+            const map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 14,
+                center: shopLocation
+            });
+
+            // Crear un marcador en la ubicación
+            const marker = new google.maps.Marker({
+                position: shopLocation,
+                map: map,
+                title: "Encuentranos justo aquí!"
+            });
+        }, function() {
+            handleLocationError(true);
+        });
+    } else {
+        handleLocationError(false);
+    }
+}
+function handleLocationError(browserHasGeolocation) {
+    alert("Error: La geolocalización falló. Asegúrate de que los permisos de locaclización estén habilitados.");
+}
+
+// Llamada inicial para cargar el mapa
+initMap();
