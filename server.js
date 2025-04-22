90007758b8d0cbcdf74690f3db11985ba7816c96
lumatamorosva <lumatamorosva@est.utn.ac.cr>
2025-04-21 21:42:25 -0600

    Se empieza a trabajar en el guardado de los formularios.

--- server.js
+++ server.js
@@ -90,3 +90,71 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+
+//Formulario:
+//Constantes
+const apiUrl2 = `https://api.github.com/repos/lumatamorosva/RecursosProyecto/contents/formularios.json`;
+
+//GET: Leer archivo JSON desde GitHub
+app.get("/formularios", async (req, res) => {
+  try {
+    const { data } = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${clave}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+    // Decodificar el base64
+    const contenidoDecodificado = Buffer.from(data.content, "base64").toString("utf8");
+    const json = JSON.parse(contenidoDecodificado);
+    res.json(json);
+  }
+  //Manejo de errores 
+  catch (error) {
+    console.error("Error al leer el archivo:", error.response?.data || error.message);
+    res.status(500).json({ error: "No se pudo leer el archivo." });
+  }
+});
+
+//PUT: Guardar nuevo formulario
+app.post("/formularios", async (req, res) => {
+  const nuevoFormulario = req.body.formulario;
+  try {
+    //Obtener contenido actual
+    const { data } = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${clave}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+    const contenidoDecodificado = Buffer.from(data.content, "base64").toString("utf8");
+    const json = JSON.parse(contenidoDecodificado);
+    if (!Array.isArray(json.formularios)) {
+      json.formularios = [];
+    }
+    json.formularios.push(nuevoFormulario );
+    //Codificar nuevo contenido
+    const nuevoContenido = JSON.stringify(json, null, 2);
+    const contentBase64 = Buffer.from(nuevoContenido, "utf8").toString("base64");
+    //Enviar actualización a GitHub
+    await axios.put(apiUrl, {
+      message: "Nuevo formulario agregado",
+      content: contentBase64,
+      sha: data.sha,
+      branch: branch,
+    }, {
+      headers: {
+        Authorization: `Bearer ${clave}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+     // Agrega el header de no-cache
+     res.set(noCacheHeaders);
+    res.json({ mensaje: "Formulario guardado con éxito" });
+  } catch (error) {
+    console.error("Error al guardar formulario:", error.response?.data || error.message);
+    res.status(500).json({ error: "No se pudo guardar el formulario." });
+  }
+});
